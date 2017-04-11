b923b7bf892467838308584d47856d1b15bb4eaa
USER <USER@Lenovo>
2017-04-11 16:05:06 -0600

    New Car Class

---
+++ TestGitProject/src/test/Car.java
@@ -0,0 +1,19 @@
+package test;
+
+
+public class Car 
+{
+    
+    private final String model;
+    
+    public Car(String pModel)
+    {
+        this.model = pModel; 
+    }
+    
+    public String getModel()
+    {
+        return this.model;
+    }
+    
+}
